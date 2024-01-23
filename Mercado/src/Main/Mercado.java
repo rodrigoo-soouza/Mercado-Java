9c5cc41476b6afc48b6ce31c1af2398746c982fe
rodrigoo-soouza <digosouzafonseca@gmail.com>
2024-01-23 20:21:32 -0300

    Inicio

---
+++ Mercado/src/Main/Mercado.java
@@ -0,0 +1,11 @@
+package Main;
+
+import Model.Produto;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Mercado {
+    private static Scanner input = new Scanner(System.in);
+    private static ArrayList<Produto> produtos;
+}
---
+++ Mercado/src/Util/Util.java
@@ -0,0 +1,16 @@
+package Util;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Util {
+    static NumberFormat numberFormat =
+            new DecimalFormat("R$ #,##0.00", new DecimalFormatSymbols(new Locale("pt", "BR")));
+
+    public static String doubleToString(Double value){
+
+        return numberFormat.format(value);
+    }
+}
